783d360f25c982e434805d6f41ca7dfdec394fd0
Kevin Xheng <kevinjone25@gmail.com>
2023-04-28 00:40:10 +0800

    feat: add user test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
+    }).useMocker((token) => {
+      const results = ['test1', 'test2'];
+      if (token === UserService) {
+        return { findAll: jest.fn().mockResolvedValue(results) };
+      }
+      if (typeof token === 'function') {
+        const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+        const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+        return new Mock();
+      }
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+
+    userService = module.get<UserService>(UserService);
+    userController = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  // describe('findUserId', () => {
+  //   it('should return an array of cats', async () => {
+  //     const result = ['test'];
+  //     jest.spyOn(userService, 'findAll').mockImplementation(() => result);
+
+  //     expect(await userController.findAll()).toBe(result);
+  //   });
+  // });
 });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,6 +6,8 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+
 
 
 }
